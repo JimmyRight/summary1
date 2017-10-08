5939b8a6b20ebe11aa8aef4640df2ce03b665a0f
hubot <1271432244@qq.com>
2017-10-08 23:10:43 +0800

    py编程
    研究方向小结；
    学习到字典。

--- Py编程.py
+++ Py编程.py
@@ -1,4 +1,12 @@
-﻿#python编程常用知识点
+﻿#Python关键技术
+flask django
+python、pypy、shell
+熟悉 http、tcp/ip 网络协议
+熟悉 Mysql、redis、twisted，精通SQL语法，有能力编写复杂SQL脚本， hadoop 、 hbase 、 spark 、 storm ， redis 集群、 mongodb 集群、 hbase 集群、 elasticsearch 集群
+数据采集、清洗、入库等ETL流程，建立数据模型，对数据进行挖掘、优化及统计
+机器学习、深度学习领域的技术研发工作，能够熟练的使用Caffe、Tensorflow、Theano、Torch、MXNet等任一种主流的深度学习框架
+有linux环境开发经验，熟练使用脚本编程进行数据处理，如：ShellPython；
+#python编程常用知识点
 循环
 数组
 选择
@@ -1388,6 +1396,268 @@ Python 列表(List)
 			>>> list_2d[2].append(7)
 			>>> list_2d
 			[[0, 0, 0, 0, 0, 3, 5], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 7], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]		
+			
+
+Python 元组
+	Python的元组与列表类似，不同之处在于元组的元素不能修改，可以看作只读列表。
+	元组使用小括号，列表使用方括号。
+	元组创建很简单，只需要在括号中添加元素，并使用逗号隔开即可。
+		如下实例：
+		tup1 = ('physics', 'chemistry', 1997, 2000);
+		tup2 = (1, 2, 3, 4, 5 );
+		tup3 = "a", "b", "c", "d";
+	
+	创建空元组
+		tup1 = ();
+	元组中只包含一个元素时，需要在元素后面添加逗号
+		tup1 = (50,);
+	访问元组
+		元组与字符串类似，下标索引从0开始，可以进行截取，组合等。
+		元组可以使用下标索引来访问元组中的值，如下实例:
+		#!/usr/bin/python
+
+		tup1 = ('physics', 'chemistry', 1997, 2000);
+		tup2 = (1, 2, 3, 4, 5, 6, 7 );
+
+		print "tup1[0]: ", tup1[0]
+		print "tup2[1:5]: ", tup2[1:5]
+		
+		以上实例输出结果：
+		tup1[0]:  physics
+		tup2[1:5]:  (2, 3, 4, 5)
+		
+	修改元组
+		元组中的元素值是不允许修改的，但我们可以对元组进行连接组合，如下实例:
+		#!/usr/bin/python
+		# -*- coding: UTF-8 -*-
+
+		tup1 = (12, 34.56);
+		tup2 = ('abc', 'xyz');
+
+		# 以下修改元组元素操作是非法的。
+		# tup1[0] = 100;
+
+		# 创建一个新的元组
+		tup3 = tup1 + tup2;
+		print tup3;
+		
+		以上实例输出结果：
+		(12, 34.56, 'abc', 'xyz')
+		
+	删除元组
+		元组中的元素值是不允许删除的，但我们可以使用del语句来删除整个元组，如下实例:
+		#!/usr/bin/python
+
+		tup = ('physics', 'chemistry', 1997, 2000);
+
+		print tup;
+		del tup;
+		print "After deleting tup : "
+		print tup;
+		
+		以上实例元组被删除后，输出变量会有异常信息，输出如下所示：
+		('physics', 'chemistry', 1997, 2000)
+		After deleting tup :
+		Traceback (most recent call last):
+		  File "test.py", line 9, in <module>
+			print tup;
+		NameError: name 'tup' is not defined
+		
+	元组运算符
+		与字符串一样，元组之间可以使用 + 号和 * 号进行运算。这就意味着他们可以组合和复制，运算后会生成一个新的元组。
+		Python 表达式	结果	描述
+		len((1, 2, 3))	3	计算元素个数
+		(1, 2, 3) + (4, 5, 6)	(1, 2, 3, 4, 5, 6)	连接
+		('Hi!',) * 4	('Hi!', 'Hi!', 'Hi!', 'Hi!')	复制
+		3 in (1, 2, 3)	True	元素是否存在
+		for x in (1, 2, 3): print x,	1 2 3	迭代
+		
+	元组索引，截取
+		因为元组也是一个序列，所以我们可以访问元组中的指定位置的元素，也可以截取索引中的一段元素，如下所示：
+		元组：
+		L = ('spam', 'Spam', 'SPAM!')
+		Python 表达式	结果	描述
+		L[2]	'SPAM!'	读取第三个元素
+		L[-2]	'Spam'	反向读取；读取倒数第二个元素
+		L[1:]	('Spam', 'SPAM!')	截取元素
+
+	无关闭分隔符
+		任意无符号的对象，以逗号隔开，默认为元组，如下实例：
+		#!/usr/bin/python
+
+		print 'abc', -4.24e93, 18+6.6j, 'xyz';
+		x, y = 1, 2;
+		print "Value of x , y : ", x,y;
+		
+		以上实例运行结果：
+		abc -4.24e+93 (18+6.6j) xyz
+		Value of x , y : 1 2
+	
+	元组内置函数
+		Python元组包含了以下内置函数
+		序号	方法及描述
+		1	cmp(tuple1, tuple2)
+		比较两个元组元素。
+		2	len(tuple)
+		计算元组元素个数。
+		3	max(tuple)
+		返回元组中元素最大值。
+		4	min(tuple)
+		返回元组中元素最小值。
+		5	tuple(seq)
+		将列表转换为元组。
+		
+
+Python 字典(Dictionary)
+	字典是另一种可变容器模型，且可存储任意类型对象。
+	字典的每个键值(key=>value)对用冒号(:)分割，每个对之间用逗号(,)分割，整个字典包括在花括号({})中 ,格式如下所示：
+		d = {key1 : value1, key2 : value2 }
+	键必须是唯一的，但值则不必。
+	值可以取任何数据类型，但键必须是不可变的，如字符串，数字或元组。
+
+	一个简单的字典实例：
+		dict = {'Alice': '2341', 'Beth': '9102', 'Cecil': '3258'}
+	也可如此创建字典：
+		dict1 = { 'abc': 456 };
+		dict2 = { 'abc': 123, 98.6: 37 };
+	
+	访问字典里的值
+		把相应的键放入方括弧，如下实例:
+		实例
+		#!/usr/bin/python
+		 
+		dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'};
+		 
+		print "dict['Name']: ", dict['Name'];
+		print "dict['Age']: ", dict['Age'];
+		
+		以上实例输出结果：
+		dict['Name']:  Zara
+		dict['Age']:  7
+	
+	如果用字典里没有的键访问数据，会输出错误如下：
+		实例
+		#!/usr/bin/python
+		 
+		dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'};
+		 
+		print "dict['Alice']: ", dict['Alice'];
+		
+		以上实例输出结果：
+		dict['Alice']: 
+		Traceback (most recent call last):
+		  File "test.py", line 5, in <module>
+			print "dict['Alice']: ", dict['Alice'];
+		KeyError: 'Alice'
+
+	修改字典
+		向字典添加新内容的方法是增加新的键/值对，修改或删除已有键/值对如下实例:
+		实例
+		#!/usr/bin/python
+		 
+		dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'};
+		 
+		dict['Age'] = 8; # update existing entry
+		dict['School'] = "DPS School"; # Add new entry
+		 
+		 
+		print "dict['Age']: ", dict['Age'];
+		print "dict['School']: ", dict['School'];
+		
+		以上实例输出结果：
+		dict['Age']:  8
+		dict['School']:  DPS School
+
+	删除字典元素
+		能删单一的元素也能清空字典，清空只需一项操作。
+		显示删除一个字典用del命令，如下实例：
+		实例
+		#!/usr/bin/python
+		# -*- coding: UTF-8 -*-
+		 
+		dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'};
+		 
+		del dict['Name']; # 删除键是'Name'的条目
+		dict.clear();     # 清空词典所有条目
+		del dict ;        # 删除词典
+		 
+		print "dict['Age']: ", dict['Age'];
+		print "dict['School']: ", dict['School'];
+		
+		但这会引发一个异常，因为用del后字典不再存在：
+		dict['Age']:
+		Traceback (most recent call last):
+		  File "test.py", line 8, in <module>
+			print "dict['Age']: ", dict['Age'];
+		TypeError: 'type' object is unsubscriptable
+		注：del()方法后面也会讨论。
+
+	字典键的特性
+		字典值可以没有限制地取任何python对象，既可以是标准的对象，也可以是用户定义的，但键不行。
+		两个重要的点需要记住：
+		1）不允许同一个键出现两次。创建时如果同一个键被赋值两次，后一个值会被记住，如下实例：
+		实例
+		#!/usr/bin/python
+		 
+		dict = {'Name': 'Zara', 'Age': 7, 'Name': 'Manni'};
+		 
+		print "dict['Name']: ", dict['Name'];
+		以上实例输出结果：
+		dict['Name']:  Manni
+		2）键必须不可变，所以可以用数字，字符串或元组充当，所以用列表就不行，如下实例：
+		实例
+		#!/usr/bin/python
+		 
+		dict = {['Name']: 'Zara', 'Age': 7};
+		 
+		print "dict['Name']: ", dict['Name'];
+		
+		以上实例输出结果：
+		Traceback (most recent call last):
+		  File "test.py", line 3, in <module>
+			dict = {['Name']: 'Zara', 'Age': 7};
+		TypeError: list objects are unhashable
+
+字典内置函数&方法
+	Python字典包含了以下内置函数：
+	序号	函数及描述
+	1	cmp(dict1, dict2)
+	比较两个字典元素。
+	2	len(dict)
+	计算字典元素个数，即键的总数。
+	3	str(dict)
+	输出字典可打印的字符串表示。
+	4	type(variable)
+	返回输入的变量类型，如果变量是字典就返回字典类型。
+	Python字典包含了以下内置方法：
+	序号	函数及描述
+	1	dict.clear()
+	删除字典内所有元素
+	2	dict.copy()
+	返回一个字典的浅复制
+	3	dict.fromkeys(seq[, val]))
+	创建一个新字典，以序列 seq 中元素做字典的键，val 为字典所有键对应的初始值
+	4	dict.get(key, default=None)
+	返回指定键的值，如果值不在字典中返回default值
+	5	dict.has_key(key)
+	如果键在字典dict里返回true，否则返回false
+	6	dict.items()
+	以列表返回可遍历的(键, 值) 元组数组
+	7	dict.keys()
+	以列表返回一个字典所有的键
+	8	dict.setdefault(key, default=None)
+	和get()类似, 但如果键不存在于字典中，将会添加键并将值设为default
+	9	dict.update(dict2)
+	把字典dict2的键/值对更新到dict里
+	10	dict.values()
+	以列表返回字典中的所有值
+	11	pop(key[,default])
+	删除字典给定键 key 所对应的值，返回值为被删除的值。key值必须给出。 否则，返回default值。
+	12	popitem()
+	随机返回并删除字典中的一对键和值。
+	
+	
+	
 			
 		
 		
